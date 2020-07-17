f926369fffecd19a93f6808f67ad70cd9e0becae
Liliia <chuiko1112@gmail.com>
2020-07-17 17:16:41 +0300

    d

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git add -f ./dist
 git commit -m "build"
-git push --delete origin gh-pages
+# git push --delete origin gh-pages
 git subtree --prefix dist push origin gh-pages
 git reset HEAD^ --soft
 rm -rf ./dist
